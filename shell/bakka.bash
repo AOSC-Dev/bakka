56e0bd27c570978748d1494859c7b5db0b7834b7
eatradish <sakiiily@aosc.io>
2022-01-25 06:09:34 +0800

    meta: set project name as bakka
    - shell/bakka.bash: use command to replace bakka path

--- shell/bakka.bash
+++ shell/bakka.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-function abbsjump {
+function bakka {
     if [[ $1 == "cd" && $2 != "--help" && $2 != "-h" ]]; then
-    	cd "$(/usr/local/bin/abbsjump cd ${2})"
+    	cd "$(command bakka cd ${2})"
     elif [[ $1 == "jump" && $2 != "--help" && $2 != "-h" ]]; then
-        cd "$(/usr/local/bin/abbsjump jump ${2})"
+        cd "$(command bakka jump ${2})"
     else
-        /usr/local/bin/abbsjump $@
+        command abbsjump $@
     fi
 }
