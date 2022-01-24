3b0c22d619f14d50c8f898d5a892def82cb7dcbc
eatradish <sakiiily@aosc.io>
2022-01-25 05:58:34 +0800

    shell/abj.bash: fix abbsjump run

--- shell/abj.bash
+++ shell/abj.bash
@@ -2,10 +2,10 @@
 
 function abbsjump {
     if [[ $1 == "cd" && $2 != "--help" && $2 != "-h" ]]; then
-    	cd "$(abbsjump cd ${2})"
+    	cd "$(/usr/local/bin/abbsjump cd ${2})"
     elif [[ $1 == "jump" && $2 != "--help" && $2 != "-h" ]]; then
-        cd "$(abbsjump jump ${2})"
+        cd "$(/usr/local/bin/abbsjump jump ${2})"
     else
-        abbsjump $@
+        /usr/local/bin/abbsjump $@
     fi
 }
