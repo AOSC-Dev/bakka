06448888b89fa68f34125407ef216b70f1741b58
eatradish <sakiiily@aosc.io>
2022-01-24 22:32:57 +0800

    shell: fix print abbsjump help message

--- shell/abj.bash
+++ shell/abj.bash
@@ -3,7 +3,7 @@
 function abbsjump {
     A=$1
     B=$2
-    if [[ $A = "cd" ]]; then
+    if [[ $A == "cd" && $B != "--help" && $B != "-h" ]]; then
     	cd "$(/home/saki/abbsjump/target/debug/abbsjump cd ${B})"
     else
         /home/saki/abbsjump/target/debug/abbsjump $@
