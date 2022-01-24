dccc4f6fa1206f34280009a656c637941d19c7f6
eatradish <sakiiily@aosc.io>
2022-01-25 07:30:59 +0800

    shell/bakka.bash: improve logic

--- shell/bakka.bash
+++ shell/bakka.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function bakka {
-    if [[ $1 == "cd" && $2 != "--help" && $2 != "-h" ]]; then
+    if [[ $1 == "cd" && $2 != -* ]]; then
     	cd "$(command bakka cd ${2})"
-    elif [[ $1 == "jump" && $2 != "--help" && $2 != "-h" ]]; then
+    elif [[ $1 == "jump" && $2 != -* ]]; then
         cd "$(command bakka jump ${2})"
     else
-        command abbsjump $@
+        command bakka $@
     fi
 }
