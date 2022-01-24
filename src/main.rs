414154e30228a69a94f6e7209539dfa69e179df3
eatradish <sakiiily@aosc.io>
2022-01-25 07:29:25 +0800

    Revert "main: set bakka cd as default feature"
    This reverts commit be41e01a257dfe90ac1e550d2ad7c48f08bb434b.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use dialoguer::{theme::ColorfulTheme, Select};
 use std::path::Path;
 
@@ -7,13 +7,22 @@ mod index;
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
 struct Args {
+    #[clap(subcommand)]
+    subcommand: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
     /// Switch to this package directory
+    Cd(CdSubCommand),
+    /// Switch to thic package directory (no match mode)
+    Jump(CdSubCommand),
+}
+
+#[derive(Parser, Debug)]
+struct CdSubCommand {
     #[clap()]
-    package: Option<String>,
-    #[clap(long, short = 'c')]
-    cd: Option<String>,
-    #[clap(long, short = 'j')]
-    jump: Option<String>,
+    package: String,
 }
 
 fn main() {
@@ -27,41 +36,29 @@ fn main() {
     let abbs_tree_path = Path::new(&abbs_tree_path);
     let index = index::read_index(abbs_tree_path).unwrap();
     let args = Args::parse();
-    if let Some(package) = args.package {
-        cd(index, package, abbs_tree_path);
-        std::process::exit(0);
-    }
-    if let Some(package) = args.cd {
-        cd(index, package, abbs_tree_path);
-        std::process::exit(0);
-    }
-    if let Some(package) = args.jump {
-        jump(index, abbs_tree_path, package);
-        std::process::exit(0);
-    }
-}
-
-fn jump(index: Vec<(String, String)>, abbs_tree_path: &Path, package: String) {
-    let count = index.iter().position(|(x, _)| x == &package);
-    match count {
-        Some(count) => println!(
-            "{}",
-            abbs_tree_path.join(index[count].1.to_owned()).display()
-        ),
-        None => {
-            eprintln!("Cannot find package: {}", package);
-            std::process::exit(1);
+    match args.subcommand {
+        Command::Cd(CdSubCommand { package }) => {
+            println!(
+                "{}",
+                abbs_tree_path
+                    .join(get_package_directory(index, package))
+                    .display()
+            );
+        }
+        Command::Jump(CdSubCommand { package }) => {
+            let count = index.iter().position(|(x, _)| x == &package);
+            match count {
+                Some(count) => println!(
+                    "{}",
+                    abbs_tree_path.join(index[count].1.to_owned()).display()
+                ),
+                None => {
+                    eprintln!("Cannot find package: {}", package);
+                    std::process::exit(1);
+                }
+            }
         }
     }
-}
-
-fn cd(index: Vec<(String, String)>, package: String, abbs_tree_path: &Path) {
-    println!(
-        "{}",
-        abbs_tree_path
-            .join(get_package_directory(index, package))
-            .display()
-    );
 }
 
 fn get_package_directory(index: Vec<(String, String)>, package: String) -> String {
